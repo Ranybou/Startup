7a0e92503cdcdeb3cb2f83a8479cc7c707384305
Ranybou <bouhlilarany@gmail.com>
2023-12-18 04:04:09 +0100

    pusharticles

---
+++ pages/articles/[slug].vue
@@ -0,0 +1,32 @@
+<script setup>
+const query = gql`
+  query article($slug: String!) {
+    article(where: { slug: $slug }) {
+      titre
+      texte {
+        html
+      }
+    }
+  }
+`;
+
+const article = ref();
+
+const route = useRoute();
+const { data } = await useAsyncQuery(query, {
+  slug: route.params.slug,
+});
+
+console.log(data.value);
+article.value = data.value.article;
+</script>
+
+<template>
+  <div v-if="articles" class="max-w-lg">
+    <h2 class="text-3xl text-center">{{ articles.titre }}</h2>
+    <div v-html="article.texte.html"></div>
+  </div>
+  <div v-else>
+    <li>Loading...</li>
+  </div>
+</template>
---
+++ pages/articles/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+const query = gql`
+query Articles {
+  articles {
+    createdAt
+    publishedAt
+    titre
+    slug
+    updatedAt
+  }
+}
+`;
+
+const articles = ref();
+const { data } = await useAsyncQuery(query);
+
+console.log(data.value);
+articles.value = data.value.articles;
+</script>
+
+<template>
+  <li v-for="article in articles">
+    <NuxtLink :to="`/articles/${article.slug}`">
+      <h2>
+        {{ article.titre }}
+      </h2>
+
+      <div v-html="produit.texte.html"></div>
+    </NuxtLink>
+  </li>
+</template>
