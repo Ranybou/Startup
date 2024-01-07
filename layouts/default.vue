f260c245d8cf0d56c4a35a8412de7e3ffbf25dc8
Ranybou <bouhlilarany@gmail.com>
2024-01-07 21:12:31 +0100

    finalcommitviewport

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <div class="min-h-screen bg-green-400">
     <div class="container mx-auto px-3 flex flex-wrap items-center justify-center text-center py-5">
    
